36ffa08e863bcc9b5d628cf2cf8a79c573ba1a02
daehee <daehee@daehee.com>
2023-04-20 13:45:35 +0000

    x

--- pwnlab-ai.py
+++ pwnlab-ai.py
@@ -1,22 +1,30 @@
 import sys
+from flask import Flask, request, jsonify
+from datetime import datetime
+import time, json
+import hashlib, os, base64, glob
+from slackeventsapi import SlackEventAdapter
+from slack_sdk.web import WebClient
 import openai
-import json
 import requests
-import time
+app = Flask(__name__)
 
-openai_key = sys.argv[1]
-slack_key = sys.argv[2]
+# Our app's Slack Event Adapter for receiving actions via the Events API
+#slack_signing_secret = os.environ["SLACK_SIGNING_SECRET"]
+#slack_events_adapter = SlackEventAdapter(slack_signing_secret, "/slack/events", app)
 
-# Use the openai_secret_manager to get your API key
-openai.api_key = openai_key
-SLACK_EVENT_ENDPOINT = "https://slack.com/api/events.listen"
-#channel_id = 'C050MENA4H4'  # general  member id: U054A375LAD
+# Create a SlackClient for your bot to use for Web API requests
+slack_bot_token = os.environ["SLACK_BOT_TOKEN"]
+print(slack_bot_token)
+#slack_client = WebClient(slack_bot_token)
+
+openai.api_key = os.environ["OPENAI_KEY"]
 
 def ask_gpt(prompt):
     response = openai.Completion.create(
-        engine="text-davinci-003",  # ChatGPT model        
+        engine="text-davinci-003",  # ChatGPT model
         prompt=prompt,
-        max_tokens=500,  # Adjust the response length
+        max_tokens=200,  # Adjust the response length
         n=1,
         stop=None,
         temperature=0.7,  # Adjust creativity (lower value = more focused, higher value = more random)
@@ -33,32 +41,45 @@ def post_slack(channel_id, message, slack_token):
             'text': message
            }
     URL = "https://slack.com/api/chat.postMessage"
+    print(data)
     res = requests.post(URL, data=data)
     print(res.text)
 
-# Define the Slack bot's token
-SLACK_BOT_TOKEN = slack_key
-# Define the Slack event listener
-SLACK_EVENT_ENDPOINT = "https://slack.com/api/events.listen"
 
-def handle_question(event_data):
-    # Extract the user's question from the event data
-    question = event_data["text"]
-    answer = ask_gpt(question + '. 짧고 간결히 반말로 답해줘.')
+@app.route('/slack/events', methods=['POST'])
+def handle_slack_events():
+    global slack_bot_token
+    print(slack_bot_token)
+    # Load the request data as JSON
+    request_data = json.loads(request.data)
 
-    # Send the answer back to the user via Slack
-    channel = event_data["channel"]
-    requests.get(SLACK_BOT_ENDPOINT % (channel, answer))
-    post_slack(channel, answer, slack_key)
+    print(request_data, file=sys.stderr)
 
-# Listen for events from Slack
-while True:
-    response = requests.post(SLACK_EVENT_ENDPOINT, json={"token": slack_key})
-    event_data = json.loads(response.text)
-    if "event" in event_data:
-        event_type = event_data["event"]["type"]
-        if event_type == "app_mention":
-            handle_question(event_data["event"])
+    # Check if the event is a challenge event
+    if 'challenge' in request_data:
+        return jsonify({'challenge': request_data['challenge']})
+    elif 'event' in request_data:
+        event_data = request_data['event']
+        # Check if the event is a message event
+        if 'type' in event_data and event_data['type'] == 'app_mention':
+            # Extract the message text
+            message_text = event_data['text']
+            if message_text.startswith('<'):
+                message_text = message_text.split(' ')[1]
+            print("Message received:", message_text)
 
-    time.sleep(1)
+            # Extract the channel ID
+            channel_id = event_data['channel']
+            print("Channel ID:", channel_id)
+
+            answer = ask_gpt(message_text + '. 짧고 간결히 반말로 답해줘.')
+            print(answer)
+            post_slack(channel_id, answer, slack_bot_token)
+            return '', 200
+        else:
+            return '', 200
+    else:
+        return '', 200
+
+app.run(host='0.0.0.0', port=3000)
 
